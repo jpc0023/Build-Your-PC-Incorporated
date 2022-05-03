d9a206584bb935b48e5e436c6835faac9f8fa7f3
Jimmy Rando <jsturat46925@gmail.com>
2022-05-02 22:07:42 -0400

    added initial query setup in typeDefs

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,33 @@
+const {gql}= require('apollo-server-express');
+
+const typeDefs= gql`
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        savedProducts: [String]
+    }
+
+    type Product {
+        _id: ID
+        name: String!
+        description: String!
+        price: Float
+        category: [String] # might need input type here instead of string
+    }
+
+    # might need authentication here
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+
+    type Mutation {
+
+    }
+
+`;
+
+module.exports= typeDefs
