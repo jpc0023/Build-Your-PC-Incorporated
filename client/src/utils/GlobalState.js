e448e327188b1a4b2d7020c8b5c5e3a6d8359235
Pao Her <p.her007@hotmail.com>
2022-05-11 15:52:05 -0500

    add untrack file so it can be push to github

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -7,10 +7,7 @@ const { Provider } = StoreContext;
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
     products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
+
   });
 
   return <Provider value={[state, dispatch]} {...props} />;
